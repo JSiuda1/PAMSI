3329dc638ac0ce9305a2d527e93beb6284b525bf
Jakub Siuda <jakkub68@gmail.com>
2022-05-15 19:54:46 +0200

    Update run.sh

--- projekt2/run.sh
+++ projekt2/run.sh
@@ -16,24 +16,6 @@ echo -e "\n \nUruchamianie programu \n \n"
 
 sleep 1
 
-if [ "$1" == "-v" ]; then
-
-  if [[ -n $3 ]] && [[ -n $2 ]]; then
-    mess_1=$1
-    mess_2=$3
-    valgrind ./projekt_1 "$mess_1" "$mess_2"
-  else 
-    valgrind ./projekt_1
-  fi
-else
-  if [[ -n $1 ]] && [[ -n $2 ]]; then
-    mess_1=$1
-    mess_2=$2
-    ./projekt_1 "$mess_1" "$mess_2"
-  else 
-    ./projekt_1
-  fi
-
-fi 
+./projekt_1 
 
 
